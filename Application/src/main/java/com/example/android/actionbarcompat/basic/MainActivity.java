bd2c99a9dc36e4e3feb76add4f8d5d0fdafb1fe3
张利强 <liqiangzhang@anjuke.com>
2014-12-18 18:55:20 +0800

    完成翻译和一些README中的说明。

--- Application/src/main/java/com/example/android/actionbarcompat/basic/MainActivity.java
+++ Application/src/main/java/com/example/android/actionbarcompat/basic/MainActivity.java
@@ -23,6 +23,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 /**
+ * 该实例展示了如何使用ActionBarCompat创建一个基本的具有action item的Activity。包含了从menu的布局文件中加载
+ * 和通过代码添加Action item。
+ *
+ * 该Activity扩展自{@link ActionBarActivity}，{@link ActionBarActivity}提供了在Android v2.1以上的机器上
+ * 展示兼容的ActionBar所需要的所有方法。
+ *
  * This sample shows you how to use ActionBarCompat to create a basic Activity which displays
  * action items. It covers inflating items from a menu resource, as well as adding an item in code.
  *
@@ -39,20 +45,27 @@ public class MainActivity extends ActionBarActivity {
 
     // BEGIN_INCLUDE(create_menu)
     /**
+     * 使用该方法来描述你的菜单，添加菜单item。
+     * 当你重新实现了该方法，并在菜单中添加了item，你需要返回true。
+     *
      * Use this method to instantiate your menu, and add your items to it. You
      * should return true if you have added items to it and want the menu to be displayed.
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate our menu from the resources by using the menu inflater.
+        //使用menu inflater 从menu布局文件中加载菜单
         getMenuInflater().inflate(R.menu.main, menu);
 
         // It is also possible add items here. Use a generated id from
         // resources (ids.xml) to ensure that all menu ids are distinct.
+        //有章可依在这里通过代码动态添加item，使用一个在ids.xml中定义的id来确保
+        //所有的菜单id都是唯一的。
         MenuItem locationItem = menu.add(0, R.id.menu_location, 0, R.string.menu_location);
         locationItem.setIcon(R.drawable.ic_action_location);
 
         // Need to use MenuItemCompat methods to call any action item related methods
+        // 需要调用MenuItemCompat里面的方法来
         MenuItemCompat.setShowAsAction(locationItem, MenuItem.SHOW_AS_ACTION_IF_ROOM);
 
         return true;
@@ -64,6 +77,9 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when one of the menu items to selected. These items
      * can be on the Action Bar, the overflow menu, or the standard options menu. You
      * should return true if you handle the selection.
+     *
+     * 该方法在菜单的item被选择时候被调用。这些item可以是在ActionBar上的，在悬浮菜单上的,
+     * 也可以是标准的option 菜单中的。如果你处理了这些选择动作，需要返回true。
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- README.md
+++ README.md
@@ -1,5 +1,51 @@
 Android ActionBarCompat-Basic Sample
 ===================================
+该实例展示了如何使用ActionBarCompat创建一个基本的具有action item的Activity。包含了从menu的布局文件中加载
+和通过代码添加Action item。
+
+介绍
+------------
+
+由于在大多数APP里面都需要一个ToolBar，从Android3.0 开始引入ActionBar操作。这种操作
+识别用户（在程序中）的位置，并提供用户的行为和导航模式。
+
+使用ActionBar将为用户提供一个在不同应用程序熟悉的界面，并优雅地适应不同的屏幕配置。
+在11以上的Android版本中，可以通过[ActionBar][1]的API实例化一个ActionBar。
+支持比较旧的设备可以使用[support library’s ActionBar][2] 来实现（向下兼容到7）。
+该示例使用Support Library里面的API。
+
+使用Support Library中的Actionbar需要以下的步骤：
+1. 通过extend 扩展库里面的ActionBarActivity创建一个Activity
+2. 使用（或extend）一个 Theme.AppCompat 中定义的主题到你的Activity上。
+
+遵从以上步骤后，所有的Action items 就会被创建，或者是在`[onCreateOptionsMenu()][3]`方法被调用时候
+创建。
+
+关键代码（by StormGens）
+---------
+在代码中为ActionBar添加菜单项：
+
+    MenuItem locationItem = menu.add(0, R.id.menu_location, 0, R.string.menu_location);
+ 
+          locationItem.setIcon(R.drawable.ic_action_location);
+          MenuItemCompat.setShowAsAction(locationItem, MenuItem.SHOW_AS_ACTION_IF_ROOM);
+          
+
+在布局文件中声明菜单：
+
+    <menu xmlns:android="http://schemas.android.com/apk/res/android"
+        xmlns:support="http://schemas.android.com/apk/res-auto" >
+        <item
+            android:id="@+id/menu_settings"
+            android:icon="@drawable/ic_action_settings"
+            android:title="@string/menu_settings"
+            support:showAsAction="never"/>
+            
+     </menu>
+
+
+Android ActionBarCompat-Basic Sample
+===================================
 
 This sample shows you how to use ActionBarCompat to create a basic Activity
 which displays action items. It covers inflating items from a menu resource,
